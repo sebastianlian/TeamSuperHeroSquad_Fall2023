428370e76499aef3aebe6189800449f0267f9c67
stang1221 <stang1@ggc.edu>
2023-10-07 16:02:00 -0400

    Added another print statement -Sophia Tang

--- src/Game.java
+++ src/Game.java
@@ -3,5 +3,6 @@ public class Game {
     public static void main(String[] args) {
         System.out.println("Hello. Welcome to the game.");
         System.out.println("This is Grizzly survival.");
+        System.out.println("It is a Zombie game.");
     }
 }
