d604a08f3b5351ab496ed29587de25e2948769e4
sebastianlian <scarmagnola@ggc.edu>
2023-11-26 13:11:19 -0500

    fix list_item

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -9,17 +9,19 @@ public class Item {
 
 
     //TODO: get rid of this constructor when parsing method is corrected
-    public Item(int id, String name, boolean type, String description) {
+
+    //TODO: get rid of this constructor when parsing method is corrected
+    public Item(int id, String name, boolean type, String effect, String description) {
         this.id = id;
         this.name = name;
         this.type = type;
         this.effect = effect;
         this.description = description;
-        //  this.quantity = quantity;
+        this.quantity = quantity;
     }
 
 
-    public Item(int id, String name, String effect, String description, boolean type, Stats stats) {
+    public Item(int id, String name, String effect, String description, int quantity, boolean type, Stats stats) {
         this.id = id;
         this.name = name;
         this.effect = effect;
